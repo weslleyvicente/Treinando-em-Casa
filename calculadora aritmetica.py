4e0ea9e9cd9069876d9d93fbe1cd5931c5aae9a9
weslley vicente <weslleyvicente373@gmail.com>
2024-08-27 20:49:24 -0300

    Add files via upload

---
+++ calculadora aritmetica.py
@@ -0,0 +1,13 @@
+soma = 1 + 1
+subtracao = 7 - 5
+multiplicacao = 4 * 4
+divisao = 10 / 5
+potencia = 7 ** 2
+
+if (soma >=1):
+  print("soma = ", soma)
+else
+print("subtração = ", subtracao)
+print("multiplicação = ", multiplicacao)
+print("divisão = ", divisao)
+print("potência = ", potencia)
