ca0196d7f149bba6baf81d9f3125a95eb223e890
weslley vicente <weslleyvicente373@gmail.com>
2024-08-28 20:29:47 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -7,3 +7,34 @@ print(nome)
 print(idade)
 print(type (altura))
 print(type( peso))
+
+
+
+
+## Informações do meu personagem
+## alt + shift e setinha pra baixo, faz copiar as linhas
+
+PLAYER ={
+    "nome": "Sync",
+    "level": 10,
+    "hp": 340,
+    "exp": 1040,
+    "dano": 500
+}
+
+##informações dos inimigos
+
+npcs = [
+    
+    { "nome": "Monstrinho", "dano": 2, "hp": 50, "exp": 5},
+    { "nome": "Monstro", "dano": 5, "hp": 100, "exp": 10},
+    { "nome": "Monstrão", "dano": 10, "hp": 150, "exp": 15},
+    { "nome": "Chefão", "dano": 25, "hp": 200, "exp": 20}
+]
+
+
+
+
+
+for index in range(1, 50, 2):
+    print(index)
